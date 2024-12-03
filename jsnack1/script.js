545eb21f2dc9e0d5d97b577d23e7365ba7801f6e
Stephan02-hub <braghesesteven@gmail.com>
2024-12-03 11:47:07 +0100

    numeri

---
+++ jsnack1/script.js
@@ -0,0 +1,10 @@
+const numero1 = prompt()
+const numero2 = prompt()
+if (numero1 > numero2){
+    console.log(numero1)
+}else if(numero2 > numero1){
+ console.log(numero2)
+}
+else (numero1 = numero2){
+    console.log(uguali)
+}
